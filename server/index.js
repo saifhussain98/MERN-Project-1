322e4476eb7afb173f017c5c3c7bd6b16af94979
saif hussain <saif.h98@hotmail.com>
2022-10-11 02:59:27 +0100

    Get request added and tested with Thunder Client extension. Get connection Success.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const UserModel = require('./models/Users');
 
 mongoose.connect(
     "mongodb+srv://saifh98:Emonh_123@saif.tfygyzr.mongodb.net/MERN-Project-1?retryWrites=true&w=majority"
     );
 
+    app.get("/getUsers", (req, res) => {
+UserModel.find({}, (err, result) => {
+    if (err) {
+        res.json(err);
+    } else {
+        res.json(result);
+    }
+});
+    });
+    
 app.listen(3001, () => {
     console.log("SERVER RUNS GREAT!");
 });
